15c6d7809dd15a9dc314e72b6aeed059f3a140d1
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-09-13 16:52:18 -0300

    Add common directive for angular Toast

---
+++ client/src/common/toastNotification/toastNotification.directive.js
@@ -0,0 +1,40 @@
+(function() {
+  'use strict';
+
+  /**
+  * @desc directive to present angular material mdToast
+  * @example <toast-notification message="message"></toast-notification>
+  */
+  angular
+    .module('toastNotification', [])
+    .directive('toastNotification', toastNotification);
+
+
+
+  toastNotification.$inject = ['$mdToast'];
+  function toastNotification($mdToast) {
+    function link(scope, element, attrs) {
+      var showSimpleToast = function(msg) {
+        if (!msg || msg === '') {
+          return false;
+        }
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent(msg)
+            .position('top right')
+            .hideDelay(3000)
+        );
+      };
+
+      scope.$watch(attrs.message, function(newValue) {
+        showSimpleToast(newValue);
+      });
+    }
+
+    return {
+      restrict: 'E',
+      link: link
+    };
+  }
+
+})();
